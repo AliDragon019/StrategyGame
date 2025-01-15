14d680eaea4f7fa9b8c55e8dfbf77f98a02d5056
AliDragon019 <139386800+AliDragon019@users.noreply.github.com>
2025-01-16 01:44:32 +0300

    Base of the game built
    Created the panels and buttons
    Made the main scripts
    Not completed Recruit algorithm

--- StrategyGame/Assets/Scripts/Master.cs
+++ StrategyGame/Assets/Scripts/Master.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Country : MonoBehaviour
+public class Master : MonoBehaviour
 {
-    public string countryName;
-    public int money;
-    public int profit;
-    public int expenses;
-    public int balance;
-
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.name = countryName;
+        
     }
 
     // Update is called once per frame
