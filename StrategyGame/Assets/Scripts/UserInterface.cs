5ed428da76d44c01e476860783db54bcd2e54446
AliDragon019 <139386800+AliDragon019@users.noreply.github.com>
2025-01-16 20:26:03 +0300

    Fixed Recruit and Disband Functions

--- StrategyGame/Assets/Scripts/UserInterface.cs
+++ StrategyGame/Assets/Scripts/UserInterface.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class UserInterface : MonoBehaviour
 {
+    int maxNumber;
+    int minNumber;
+
+    string currentMethod;
+
     public Country country;
     public Province currentProvince;
 
@@ -53,10 +58,11 @@ public class UserInterface : MonoBehaviour
     public void CheckNumber(){
         InputField input = numberInput.GetComponent<InputField>();
         int number = int.Parse(input.text);
-        if(number > country.money){
-            input.text = (country.money).ToString();
-        } else if(number < 0){
-            input.text = "0";
+
+        if(number > maxNumber){
+            input.text = maxNumber.ToString();
+        } else if(number < minNumber){
+            input.text = minNumber.ToString();
         }
     }
 
@@ -83,6 +89,21 @@ public class UserInterface : MonoBehaviour
         ShowObject(actionsPanel);
         HideObject(approvePanel);
         HideObject(numberInput);
+
+        int number = int.Parse(numberInput.GetComponent<InputField>().text);
+        switch (currentMethod){
+            case "recruit":
+            country.money -= number;
+            currentProvince.army += number;
+            break;
+
+            case "disband":
+            country.money += number;
+            currentProvince.army -= number;
+            break;
+            
+            default: break;
+        }
     }
 
     public void Cancel(){
@@ -99,11 +120,27 @@ public class UserInterface : MonoBehaviour
         HideObject(actionsPanel);
         ShowObject(approvePanel);
         ShowObject(numberInput);
+        
+        currentMethod = "recruit";
+        minNumber = 0;
+        maxNumber = country.money;
+        
+
+        numberInput.GetComponent<InputField>().text = minNumber.ToString();
         numberInput.GetComponent<InputField>().Select();
     }
 
     public void Disband(){
-
+        HideObject(actionsPanel);
+        ShowObject(approvePanel);
+        ShowObject(numberInput);
+        
+        currentMethod = "disband";
+        minNumber = 0;
+        maxNumber = currentProvince.army;
+        
+        numberInput.GetComponent<InputField>().text = minNumber.ToString();
+        numberInput.GetComponent<InputField>().Select();
     }
     
     public void Build(){
