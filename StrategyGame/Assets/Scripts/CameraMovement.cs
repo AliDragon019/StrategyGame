c75372a76587ebe2468f3959a34df9315a5a731b
Tigre-Jaune <abdulsamadalhassan2@gmail.com>
2025-01-18 20:13:36 +0300

    Camera Movement
    Camera Movement with WASD

---
+++ StrategyGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float speed = 1f;    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) ){
+            transform.position += speed * Vector3.up * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) ){
+            transform.position += speed * Vector3.left * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ){
+            transform.position += speed * Vector3.right * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ){
+            transform.position += speed * Vector3.down * Time.deltaTime;
+        }                
+    }
+}
